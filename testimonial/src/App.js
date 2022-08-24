89045dda40d6317356a9c83b9302af2ccc9f4d32
Nicholas-Kipkoech <nickey968@gmail.com>
2022-08-24 10:58:01 +0300

    feat:loading feature

--- testimonial/src/App.js
+++ testimonial/src/App.js
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from "react";
+import Spinner from "react-bootstrap/Spinner";
 import "./App.css";
 
 export default function App() {
   const baseUrl = "https://testimonialapi.toolcarton.com/api";
 
   const [testimonials, setTestimonials] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const fetchTestimonials = async () => {
     await fetch(baseUrl)
       .then((response) => response.json())
       .then((data) => setTestimonials(data));
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -18,23 +21,33 @@ export default function App() {
 
   return (
     <div className="App">
-      <h3 className="testimonial-header">Testimonials</h3>
       <div className="container">
-        {testimonials.map((testimonial, id) => (
-          <div className="testimonials">
-            <img
-              alt={testimonial.name}
-              className="image"
-              height="150"
-              src={testimonial.avatar}
-            />
-            <div key={testimonial.id} className="User-names">
-              {testimonial.name}{" "}
-            </div>
-            <div className="message">{testimonial.message}</div>
-            <audio className="audio" controls src={testimonial.audio} />
-          </div>
-        ))}
+        <h3 className="testimonial-header">Testimonials</h3>
+        <div className="containers">
+          {loading ? (
+            <Spinner
+              animation="border"
+              className="spinner"
+              role="status"
+            ></Spinner>
+          ) : (
+            testimonials.map((testimonial, id) => (
+              <div className="testimonials">
+                <img
+                  alt={testimonial.name}
+                  className="image"
+                  height="150"
+                  src={testimonial.avatar}
+                />
+                <div key={testimonial.id} className="User-names">
+                  {testimonial.name}{" "}
+                </div>
+                <div className="message">{testimonial.message}</div>
+                <audio className="audio" controls src={testimonial.audio} />
+              </div>
+            ))
+          )}
+        </div>
       </div>
     </div>
   );
